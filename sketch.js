c9d1bbb55ac065691846e3ad3f72e918a05d1e3b
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-01-31 19:06:17 +0000

    for day 2 starter code

--- sketch.js
+++ sketch.js
@@ -1,8 +1,26 @@
 function setup(){
-    createCanvas(600,400);
-    background(0);
+    createCanvas(600, 400);
 }
 
 function draw(){
+    background(0);
+    loadPixels();
     
+    for(var row=0; row<height; row++){
+        for(var col=0; col<width; col++){
+            var index = (row * width + col) * 4;
+            
+            r = pixels[index];
+            g = pixels[index + 1];
+            b = pixels[index + 2];
+            a = pixels[index + 3];
+            
+            pixels[index] = col;
+            pixels[index + 1] = 0;
+            pixels[index + 2] = row;
+            pixels[index + 3] = a;
+        }
+    }
+    
+    updatePixels();
 }
